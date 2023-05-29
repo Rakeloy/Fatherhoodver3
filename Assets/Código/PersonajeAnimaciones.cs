4bc2b62f85c63e557c5a32fd24dfa8dcb9f6e890
Rakeloy <raquel.oya.v@gmail.com>
2023-05-29 19:31:42 +0200

    Ya están poses en reposo

--- Assets/Código/PersonajeAnimaciones.cs
+++ Assets/Código/PersonajeAnimaciones.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PersonajeAnimaciones : MonoBehaviour
 {
+    [SerializeField] private string layerIdle;
+    [SerializeField] private string layerCaminar;
     private Animator _animator;
     private PersonajeMovimiento _personajeMovimiento;
 
@@ -22,6 +24,7 @@ public class PersonajeAnimaciones : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ActualizarLayer();
         if (_personajeMovimiento.EnMovimiento == false){
             return;
         }
@@ -29,5 +32,22 @@ public class PersonajeAnimaciones : MonoBehaviour
 
         _animator.SetFloat(name:"X", _personajeMovimiento.DireccionMovimiento.x);
         _animator.SetFloat(name:"Y", _personajeMovimiento.DireccionMovimiento.y);
+    }
+
+    private void ActivarLayer(string nombreLayer){
+        for (int i = 0; i < _animator.layerCount; i++){
+            _animator.SetLayerWeight(i, weight:0);
+        }
+        _animator.SetLayerWeight(_animator.GetLayerIndex(nombreLayer), weight:1);
+    }
+
+    private void ActualizarLayer(){
+        if (_personajeMovimiento.EnMovimiento){
+            ActivarLayer(layerCaminar);
+        }
+        else
+        {
+            ActivarLayer(layerIdle);
+        }
     }
 }
