0173d9bfd7f3570434775f718f512d8932ac82ab
sandriicola <sandra.sansan16@gmail.com>
2023-05-31 19:52:58 +0200

    Paneles mensaje POPUP

--- Assets/Código/Minijuegos/Cama/EscenaCama.cs
+++ Assets/Código/Minijuegos/Cama/EscenaCama.cs
@@ -4,10 +4,26 @@ using UnityEngine.SceneManagement;
 public class EscenaCama : MonoBehaviour
 {
     public string Cama; // Nombre de la escena a la que quieres ir
+    public string Linterna; // ID del item necesario (en este caso, la linterna)
+    public GameObject panelMensaje;
+     private void Start(){
+        panelMensaje.SetActive(false);
+     }
+
 
     private void OnMouseDown()
     {
-        // Carga la escena especificada cuando se hace clic en el objeto
+       if (Inventario.Instance.TieneItem(Linterna)){
         SceneManager.LoadScene(Cama);
+       }else
+        {
+            // Mostrar el panel de mensaje indicando que se requiere el item
+            panelMensaje.SetActive(true);
+            Invoke("DesactivarPanelMensaje", 2f);
+        }
+    }
+    private void DesactivarPanelMensaje()
+    {
+        panelMensaje.SetActive(false);
     }
 }
--- Assets/Código/Minijuegos/Puerta/EscenaPuerta.cs
+++ Assets/Código/Minijuegos/Puerta/EscenaPuerta.cs
@@ -4,10 +4,24 @@ using UnityEngine.SceneManagement;
 public class EscenaPuerta : MonoBehaviour
 {
     public string Puerta; // Nombre de la escena a la que quieres ir
-
+    public string PaloGancho;
+    public GameObject panelMensaje;
+     private void Start(){
+        panelMensaje.SetActive(false);
+     }
     private void OnMouseDown()
     {
-        // Carga la escena especificada cuando se hace clic en el objeto
+        if (Inventario.Instance.TieneItem(PaloGancho)){
         SceneManager.LoadScene(Puerta);
+        }else
+        {
+            // Mostrar el panel de mensaje indicando que se requiere el item
+            panelMensaje.SetActive(true);
+            Invoke("DesactivarPanelMensaje", 2f);
+        }
+    }
+    private void DesactivarPanelMensaje()
+    {
+        panelMensaje.SetActive(false);
     }
 }
